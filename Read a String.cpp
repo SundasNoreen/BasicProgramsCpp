c384eaea2bd884321b9385e5f4bfc5de586d60c3
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-10-16 00:19:20 +0500

    Update Read a String.cpp

--- Read a String.cpp
+++ Read a String.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	//declaring string (character array)
+	//declaring string (character array) One Way
 	char string_array[100];
-	
-	cout<<"Enter a string: ";
+	cout<<"Enter first string: ";
 	cin.getline(string_array,100);
+	cout<<"First input string is: "<<string_array<<endl;
 	
-	
-	cout<<"Input string is: "<<string_array<<endl;
+	// Declaring String Datatype itself by including string in the header // Seconf Way
+	string a;
+	cout<<"Enter second string: ";
+	cin>>a;
+	cout<<"Second input string is: "<<a<<endl;
 	
 	return 0;
 }
